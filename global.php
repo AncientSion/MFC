45cc8832f1d3ab03adce5975f67835781623145e
AncientSion <rolandthegunslinger@web.de>
2018-04-09 21:02:57 +0200

    1
    1

--- global.php
+++ global.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-include(__DIR__."\debug.php");
+include(__DIR__."/debug.php");
 
 
 function getCardDataSet($name, $data){
